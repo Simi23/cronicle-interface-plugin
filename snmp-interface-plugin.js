c7ac228248ee43bd281581897e46146874def105
Simon Tamás <44925919+Simi23@users.noreply.github.com>
2024-04-29 13:30:29 +0200

    Update snmp-interface-plugin.js

--- snmp-interface-plugin.js
+++ snmp-interface-plugin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Cisco Interface SNMP state toggle
+// Cisco Interface SNMP state toggle V2C
 // Copyright (c) 2024 Tamas Simon
 
 // Dependencies to be installed: net-snmp
@@ -24,8 +24,6 @@ var snmp = require ("net-snmp");
 const IPV4REGEX = new RegExp('^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$');
 const RANGEREGEX = new RegExp('^(?:[0-9]+(?:\-[0-9]+)?,?)+$');
 
-const engineID = "8000B983805C5CA57BAFB2CAD716E0480B"
-
 // setup stdin / stdout streams 
 process.stdin.setEncoding('utf8');
 process.stdout.setEncoding('utf8');
@@ -48,38 +46,8 @@ stream.on('json', async function(job) {
 		return;
 	}
 	var interfaceIdString = params.interface_ids;
-	
-	var snmpOptions = {
-		port: 161,
-		retries: 1,
-		timeout: 5000,
-		transport: "udp4",
-		trapPort: 162,
-		version: snmp.Version3,
-		engineID: engineID, // where the X's are random hex digits
-		backwardsGetNexts: true,
-		reportOidMismatchErrors: false,
-		idBitsSize: 32,
-		context: ""
-	};
-	
-	var authLevel = getAuthLevel(params.snmp_security_level);
-	var authProto = getAuthProtocol(params.snmp_auth_proto);
-	var privProto = getPrivProtocol(params.snmp_priv_proto);
 	var enabledVal = getEnabledValue(params.enabled);
-
-	// Example user
-	var snmpUser = {
-		name: params.snmp_username,
-		level: authLevel,
-		authProtocol: authProto,
-		authKey: params.snmp_auth_key,
-		privProtocol: privProto,
-		privKey: params.snmp_priv_key
-	};
-
-	var session = snmp.createV3Session (deviceIp, snmpUser, snmpOptions);
-	
+	var session = snmp.createSession(deviceIp, params.snmp_community);
 	var varbinds = [];
 	
 	var interfaceIds = [];
@@ -128,43 +96,6 @@ async function SetSNMP(session, varbinds, stream) {
 			resolve();
 		});
 	});	
-}
-
-function getAuthLevel(authLevel) {
-	switch(authLevel) {
-		case "noAuthNoPriv":
-			return snmp.SecurityLevel.noAuthNoPriv;
-		case "authNoPriv":
-			return snmp.SecurityLevel.authNoPriv;
-		case "authPriv":
-			return snmp.SecurityLevel.authPriv;
-	}
-}
-
-function getAuthProtocol(authProto) {
-	switch(authProto) {
-		case "md5":
-			return snmp.AuthProtocols.md5;
-		case "sha":
-			return snmp.AuthProtocols.sha;
-		case "sha256":
-			return snmp.AuthProtocols.sha256;
-		case "sha512":
-			return snmp.AuthProtocols.sha512;
-	}
-}
-
-function getPrivProtocol(privProto) {
-	switch(privProto) {
-		case "des":
-			return snmp.PrivProtocols.des;
-		case "aes":
-			return snmp.PrivProtocols.aes;
-		case "aes256b":
-			return snmp.PrivProtocols.aes256b;
-		case "aes256r":
-			return snmp.PrivProtocols.aes256r;
-	}
 }
 
 function getEnabledValue(enabled) {
